ecef2dd8d2307700fdf7c98d7446aed36e526f3b
euanmitchell <euanmitchell12@gmail.com>
2021-12-31 19:15:26 +0000

    Create 1-Intro.js

---
+++ 1-Intro.js
@@ -0,0 +1,24 @@
+// This is a comment in JavaScript
+print('Hello World');
+// variables
+var city = 'Edinburgh';
+var country = 'Scotland';
+var population = 540000
+print(city, country);
+// Lists
+var largeCities = ['Tokyo', 'Delhi', 'Shanghai', 'Mexico City', 'Sao Paulo'];
+print(largeCities);
+// Dictionaries
+var cityData = {
+  'city': city,
+  'population': population,
+  'elevation': 150
+};
+print(cityData);
+print(cityData.population);
+print(cityData.elevation);
+// Functions
+var greet = function(name) {
+  return 'Hello ' + name + ', nice to meet you!';
+};
+print(greet('Euan'));
